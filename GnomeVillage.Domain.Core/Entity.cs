34dbe6a2b7ee7d555cf14203660517bf08abf263
José Luis del Río <jose-luis.delrio.altran@axa-groupsolutions.com>
2020-02-02 16:11:32 +0100

    wip

---
+++ GnomeVillage.Domain.Core/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GnomeVillage.Domain.Core
+{
+   public abstract class Entity<TKey>
+   {
+      public TKey Id { get; }
+
+      public IList<BrokenRule> BrokenRules { get; }
+
+      protected abstract void EnsureValidState();
+
+      protected void Validate(string message)
+      {
+         EnsureValidState();
+         if (BrokenRules.Count> 0)
+         {
+            throw new DomainException(message, BrokenRules);
+         }
+      }
+
+   }
+}
--- GnomeVillage.Domain/Habitant.cs
+++ GnomeVillage.Domain/Habitant.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using GnomeVillage.Domain.Core;
+using System;
 using System.Collections.Generic;
 
 namespace GnomeVillage.Domain
 {
-   public class Habitant
+   public class Habitant: Entity<HabitantId>
    {
-      public HabitantId Id { get; set; }
       public HabitantName Name { get; set; }
       public Uri Thumbnail { get; set; }
       public int Age { get; }
@@ -14,5 +14,33 @@ namespace GnomeVillage.Domain
       public HairColor HairColor { get; set; }
       public IList<Profession> Professions { get; set; }
       public IList<HabitantName> Friends { get; set; }
+
+      protected override void EnsureValidState()
+      {
+         if (Name == null || (Name!= null && string.IsNullOrEmpty(Name.Value)))
+         {
+            BrokenRules.Add(new BrokenRule("Name has to be defined"));
+         }
+
+         if (Thumbnail == null || (Name != null && Thumbnail.IsWellFormedOriginalString()))
+         {
+            BrokenRules.Add(new BrokenRule("Thumbnail has to be defined"));
+         }
+
+         if (Weight < 0)
+         {
+            BrokenRules.Add(new BrokenRule("Weight has to be a positive number"));
+         }
+
+         if (Height < 0)
+         {
+            BrokenRules.Add(new BrokenRule("Height has to be a positive number"));
+         }
+
+         if (HairColor == null)
+         {
+            BrokenRules.Add(new BrokenRule("HairColor has to be defined"));
+         }
+      }
    }
 }
