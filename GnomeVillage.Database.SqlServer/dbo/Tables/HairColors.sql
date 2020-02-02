42f46409aebcd1264f3c0ad54963ec4e54006646
José Luis del Río <jose-luis.delrio.altran@axa-groupsolutions.com>
2020-02-02 23:40:19 +0100

    wip

--- GnomeVillage.Database.SqlServer/dbo/Tables/HairColors.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[HairColors] (
-    [Name] NCHAR (200) NOT NULL,
-    CONSTRAINT [PK_HairColors] PRIMARY KEY CLUSTERED ([Name] ASC)
-);
-
--- GnomeVillage.Database.SqlServer/dbo/Tables/Professions.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Professions] (
-    [Name] NCHAR (200) NOT NULL,
-    CONSTRAINT [PK_Professions] PRIMARY KEY CLUSTERED ([Name] ASC)
-);
-
